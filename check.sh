7059e3061926f1c93e4462223208993e45526e99
Mateusz Bączek <mateusz.baczek@creotech.pl>
2025-07-04 23:12:53 +0200

    Add separate mkdocs plugin tests

--- check.sh
+++ check.sh
@@ -14,6 +14,9 @@ python -m mypy .
 # Run unit and integration tests
 python test_reqsnake.py
 
+# Run unit and integration tests
+python test_mkdocs_plugin.py
+
 # Run ruff linter
 python -m ruff check .
 
